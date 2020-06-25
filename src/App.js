7497c1787cf379ff5e36561b52f016b6103e2cd3
Matt Caldwell <cwe11@yahoo.com>
2020-06-24 19:46:37 -0600

    Initial splash component coded.  Moving to navigation menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
 
+// IMPORT CHILD COMPONENTS
+import { Splash } from './Components/Splash/Splash.js';
+
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
   }
 
   render() {
 
     return (
       <div className="App">
-
+        <Splash />
       </div>
     );
   }
---
+++ src/Components/Splash/Splash.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Splash.css';
+
+// IMPORT ASSETS
+
+
+export function Splash(props) {
+
+
+  return(
+    <section className='splash'>
+      <div className='splash_cover'></div>
+      <section className='splash_text_container'>
+        <h1>Matt Caldwell</h1>
+        <span>Front End | Developer</span>
+      </section>
+    </section>
+  );
+};
