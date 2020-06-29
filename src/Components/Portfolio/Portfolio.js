6d0058965ece039789279f1263965bd8f84bd80f
Matt Caldwell <cwe11@yahoo.com>
2020-06-29 07:46:23 -0600

    Initial code for work and portfolio sections.  TODO: finalize design

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import './Portfolio.css';
 
+const portfolio_projects = [
+  {
+    name: 'Hero Adventure',
+    pic_image: 'adventure.png',
+    project_desc: 'Hero Adventure',
+    project_type: 'School Project'
+  },
+  {
+    name: 'FESC',
+    pic_image: 'fesc.png',
+    project_desc: 'Redesign for Florida Energy Consortium',
+    project_type: 'School Project'
+  },
+  {
+    name: 'Imos Pizza',
+    pic_image: 'imos.png',
+    project_desc: 'Imos pizza copy',
+    project_type: 'Personal Learning Project'
+  },
+  {
+    name: 'Rocky Mountain Conservancy',
+    pic_image: 'romocon.png',
+    project_desc: 'Rocky Mountain Conservancy',
+    project_type: 'Personal Learning Project'
+  },
+  {
+    name: 'Rocky Mountain Conservancy',
+    pic_image: 'romocon.png',
+    project_desc: 'Rocky Mountain Conservancy',
+    project_type: 'Personal Learning Project'
+  },
+  {
+    name: 'Rocky Mountain Conservancy',
+    pic_image: 'romocon.png',
+    project_desc: 'Rocky Mountain Conservancy',
+    project_type: 'Personal Learning Project'
+  },
+]
+
 export function Portfolio(props) {
+
+  const display_projects = portfolio_projects.map(project => {
+    return(
+      <section className='portfolio_card'>
+        <h3>{project.name}</h3>
+        <img src={'../../Assets/Images/Portfolio/' + project.pic_image} / >
+      </section>
+    )
+  })
 
   return(
     <article id='portfolio' className='site_article'>
-      Portfolio
+      {display_projects}
     </article>
   )
 }
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,11 +1,62 @@
 import React from 'react';
 import './Work.css';
 
+const work_projects = [
+  {
+    name: 'Centene HQ',
+    pic_image: 'centene.png',
+    project_desc: 'Centene HQ',
+    project_location: 'Clayton, MO'
+  },
+  {
+    name: 'Denver Art Museum',
+    pic_image: 'dam.png',
+    project_desc: 'Denver Art Museum',
+    project_location: 'Denver, CO'
+  },
+  {
+    name: 'DIA - Westin Hotel',
+    pic_image: 'Westin-DIA.png',
+    project_desc: 'Westin DIA',
+    project_location: 'Denver, CO'
+  },
+  {
+    name: 'Wolfpoint East',
+    pic_image: 'wolfpoint_east.png',
+    project_desc: 'Wolfpoint East',
+    project_location: 'Chicago, IL'
+  },
+  {
+    name: 'DIA - Westin Hotel',
+    pic_image: 'Westin-DIA.png',
+    project_desc: 'Westin DIA',
+    project_location: 'Denver, CO'
+  },
+  {
+    name: 'DIA - Westin Hotel',
+    pic_image: 'Westin-DIA.png',
+    project_desc: 'Westin DIA',
+    project_location: 'Denver, CO'
+  },
+]
+
+
+
 export function Work(props) {
+
+  const display_projects = work_projects.map(project => {
+    return(
+      <section className='work_card'>
+        <h3>{project.name}</h3>
+        <img src={'../../Assets/Images/Work/' + project.pic_image} / >
+      </section>
+    )
+  })
+
 
   return(
     <article id='work' className='site_article'>
-      Work
+      {display_projects}
     </article>
   )
 }
