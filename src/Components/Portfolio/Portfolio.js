c659276a3162f64c03d9de56e191c715497b8ad5
Matt Caldwell <cwe11@yahoo.com>
2020-07-05 09:02:11 -0600

    Working on portfolio and work sections.  Background images and styles working.  TODO: finalize styles

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import './Portfolio.css';
 
+// IMPORT ASSETS
+import adventure from '../../Assets/Images/Portfolio/adventure.png';
+import fesc from '../../Assets/Images/Portfolio/fesc.png';
+import imos from '../../Assets/Images/Portfolio/imos.png';
+import romocon from '../../Assets/Images/Portfolio/romocon.png';
+
 const portfolio_projects = [
   {
     name: 'Hero Adventure',
-    pic_image: 'adventure.png',
+    pic_image: adventure,
     project_desc: 'Hero Adventure',
     project_type: 'School Project'
   },
   {
     name: 'FESC',
-    pic_image: 'fesc.png',
+    pic_image: fesc,
     project_desc: 'Redesign for Florida Energy Consortium',
     project_type: 'School Project'
   },
   {
     name: 'Imos Pizza',
-    pic_image: 'imos.png',
+    pic_image: imos,
     project_desc: 'Imos pizza copy',
     project_type: 'Personal Learning Project'
   },
   {
     name: 'Rocky Mountain Conservancy',
-    pic_image: 'romocon.png',
+    pic_image: romocon,
     project_desc: 'Rocky Mountain Conservancy',
     project_type: 'Personal Learning Project'
   },
   {
     name: 'Rocky Mountain Conservancy',
-    pic_image: 'romocon.png',
+    pic_image: romocon,
     project_desc: 'Rocky Mountain Conservancy',
     project_type: 'Personal Learning Project'
   },
   {
     name: 'Rocky Mountain Conservancy',
-    pic_image: 'romocon.png',
+    pic_image: romocon,
     project_desc: 'Rocky Mountain Conservancy',
     project_type: 'Personal Learning Project'
   },
@@ -43,10 +49,15 @@ const portfolio_projects = [
 export function Portfolio(props) {
 
   const display_projects = portfolio_projects.map(project => {
+
+    let portfolio_styles = {
+      backgroundImage: `url(${project.pic_image})`
+    }
+
     return(
-      <section className='portfolio_card'>
+      <section className='portfolio_card' style={portfolio_styles}>
         <h3>{project.name}</h3>
-        <img src={'../../Assets/Images/Portfolio/' + project.pic_image} / >
+        
       </section>
     )
   })
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import './Work.css';
 
+// IMPORT ASSETS
+import centene from '../../Assets/Images/Work/centene.jpg';
+import dam from '../../Assets/Images/Work/dam.jpg';
+import dia from '../../Assets/Images/Work/dia.jpeg';
+import wolfpoint from '../../Assets/Images/Work/wolfpoint_east.jpg';
+
 const work_projects = [
   {
     name: 'Centene HQ',
-    pic_image: 'centene.png',
+    pic_image: centene,
     project_desc: 'Centene HQ',
     project_location: 'Clayton, MO'
   },
   {
     name: 'Denver Art Museum',
-    pic_image: 'dam.png',
+    pic_image: dam,
     project_desc: 'Denver Art Museum',
     project_location: 'Denver, CO'
   },
   {
     name: 'DIA - Westin Hotel',
-    pic_image: 'Westin-DIA.png',
+    pic_image: dia,
     project_desc: 'Westin DIA',
     project_location: 'Denver, CO'
   },
   {
     name: 'Wolfpoint East',
-    pic_image: 'wolfpoint_east.png',
+    pic_image: wolfpoint,
     project_desc: 'Wolfpoint East',
     project_location: 'Chicago, IL'
   },
   {
     name: 'DIA - Westin Hotel',
-    pic_image: 'Westin-DIA.png',
+    pic_image: dia,
     project_desc: 'Westin DIA',
     project_location: 'Denver, CO'
   },
   {
     name: 'DIA - Westin Hotel',
-    pic_image: 'Westin-DIA.png',
+    pic_image: dia,
     project_desc: 'Westin DIA',
     project_location: 'Denver, CO'
   },
@@ -45,10 +51,15 @@ const work_projects = [
 export function Work(props) {
 
   const display_projects = work_projects.map(project => {
+
+    let portfolio_styles = {
+      backgroundImage: `url(${project.pic_image})`
+    }
+
     return(
-      <section className='work_card'>
+      <section className='work_card' style={portfolio_styles}>
         <h3>{project.name}</h3>
-        <img src={'../../Assets/Images/Work/' + project.pic_image} / >
+
       </section>
     )
   })
