47fd6d2c4c1470a55dc06b208dda83f3a238a240
Matt Caldwell <cwe11@yahoo.com>
2020-07-11 19:45:05 -0600

    Skills code done.  TODO: finalize design

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -21,12 +21,55 @@ const workExperience = [
 ];
 
 const techSkills = [
-  'HTML5',
-  'CSS3',
-  'Javascript',
-  'React JS',
-  'PHP',
+  {
+    skill: 'HTML5',
+    level: 3,
+    school: 'UW'
+  },
+  {
+    skill: 'CSS3',
+    level: 3,
+    school: 'UW'
+  },
+  {
+    skill: 'Javascript',
+    level: 3,
+    school: 'UW / Self'
+  },
+  {
+    skill: 'React JS',
+    level: 2,
+    school: 'Udemy / Self'
+  },
+  {
+    skill: 'PHP',
+    level: 1,
+    school: 'Udemy'
+  },
+  {
+    skill: 'MySQL',
+    level: 1,
+    school: 'Udemy'
+  },
+  {
+    skill: 'Microsoft Office',
+    level: 3,
+    school: 'MSU'
+  },
+  {
+    skill: 'Adobe Photoshop',
+    level: 2,
+    school: 'Self'
+  }
 ];
+
+const starFill = <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>
+
+const starEmpty = <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-star" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+</svg>
 
 let displaySchool = workExperience.map(school => {
   return(
@@ -42,12 +85,22 @@ let displaySchool = workExperience.map(school => {
 });
 
 let displaySkills = techSkills.map(skill => {
+
+  let starsLevel = [];
+  for(let i=0; i<4; i++) {
+    i < skill.level ? starsLevel.push(starFill) : starsLevel.push(starEmpty);
+  }
+
   return(
-    <ul className='skills'>
-      <li>
-        {skill}
-      </li>
-    </ul>
+    <p className='skill_item'>
+      {skill.skill}
+      <p className='skill_item_sub_menu'>
+        <span>Skill: {skill.skill}</span>
+        <span>{starsLevel}</span>
+        <span>Where: {skill.school}</span>
+      </p>
+    </p>
+
   )
 })
 
@@ -58,11 +111,15 @@ export function Skills(props) {
       <h1>Where and what I've learned.</h1>
       <section className='skills_row'>
         <section className='education'>
+        <h2> Where I've Learned</h2>
           {displaySchool}
         </section>
-        <section className='tech_skills'>
-          {displaySkills}
-        </section>
+        <aside className='tech_skills'>
+          <h2> What I know </h2>
+          <section className='tech_skill_items'>
+            {displaySkills}
+          </section>
+        </aside>
       </section>
     </article>
   )
