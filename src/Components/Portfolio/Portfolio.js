744db307e773f07d96c6ec9d2b9efe173c9bab04
Matt Caldwell <cwe11@yahoo.com>
2020-07-12 18:49:58 -0600

    Coded sub menu to display information for portfolio projects

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -11,38 +11,50 @@ const portfolio_projects = [
   {
     name: 'Hero Adventure',
     pic_image: adventure,
-    project_desc: 'Hero Adventure',
-    project_type: 'School Project'
+    project_desc: 'JS based adventure game',
+    project_type: 'School Project',
+    code_link: '#',
+    site_link: '#'
   },
   {
     name: 'FESC',
     pic_image: fesc,
-    project_desc: 'Redesign for Florida Energy Consortium',
-    project_type: 'School Project'
+    project_desc: 'Florida Energy Consortium Redesign',
+    project_type: 'School Project',
+    code_link: '#',
+    site_link: '#'
   },
   {
     name: 'Imos Pizza',
     pic_image: imos,
-    project_desc: 'Imos pizza copy',
-    project_type: 'Personal Learning Project'
+    project_desc: 'Copy of existing site using imospizza.com as template (site not affiliated with Imos)',
+    project_type: 'Personal Learning Project',
+    code_link: '#',
+    site_link: '#'
   },
   {
     name: 'Rocky Mountain Conservancy',
     pic_image: romocon,
     project_desc: 'Rocky Mountain Conservancy',
-    project_type: 'Personal Learning Project'
+    project_type: 'Personal Learning Project',
+    code_link: '#',
+    site_link: '#'
   },
   {
     name: 'Rocky Mountain Conservancy',
     pic_image: romocon,
     project_desc: 'Rocky Mountain Conservancy',
-    project_type: 'Personal Learning Project'
+    project_type: 'Personal Learning Project',
+    code_link: '#',
+    site_link: '#'
   },
   {
     name: 'Rocky Mountain Conservancy',
     pic_image: romocon,
     project_desc: 'Rocky Mountain Conservancy',
-    project_type: 'Personal Learning Project'
+    project_type: 'Personal Learning Project',
+    code_link: '#',
+    site_link: '#'
   },
 ]
 
@@ -57,11 +69,14 @@ export function Portfolio(props) {
 
     return(
       <section className='portfolio_card' style={sectionStyles}>
-        <a href='#' target='_blank'>
-          <h3>{project.name}</h3>
-          <h4>{project.project_desc}</h4>
-          <h4>{project.project_type}</h4>
-        </a>
+        <a href={project.site_link} target='_blank'></a>
+        <ul className='portfolio_card_information'>
+          <li>{project.name}</li>
+          <li>{project.project_desc}</li>
+          <li>{project.project_type}</li>
+          <li><a href={project.code_link}>View Code</a></li>
+          <li><a href={project.site_link}>View Site</a></li>
+        </ul>
       </section>
     )
   })
